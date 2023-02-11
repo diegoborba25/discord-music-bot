c9a2d792cc1d865a757da3b9450d112c8b410ed0
Diego Borba <diegoborba25.contato@gmail.com>
2023-02-11 13:05:27 -0300

    "Require" fixed

--- commands/help.js
+++ commands/help.js
@@ -1,5 +1,5 @@
 const { EmbedBuilder } = require('discord.js')
-const config = require('./../config.json')
+const config = require('../config.json')
 
 const { getCommandInfo, getMessage, getResources } = require('../language')
 
